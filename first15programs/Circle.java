3479fea21f49ef5607f3ef50fe7bfbd9678276e2
ASWANA B KRISHNA <aswanabkrishna@gmail.com>
2021-05-26 12:58:31 +0530

    msg1

---
+++ first15programs/Circle.java
@@ -0,0 +1,11 @@
+class Circle{
+public static void main(String args[])
+{
+double r=7.5;
+double area,perimeter;
+area=3.14*r*r;
+perimeter=2*3.14*r;
+System.out.println("Area="+area);
+System.out.println("Perimeter="+perimeter);
+}
+}
