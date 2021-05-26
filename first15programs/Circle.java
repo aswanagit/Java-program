a1cd6e4d3a76fbe296da4f544308738fa29599c3
ASWANA B KRISHNA <aswanabkrishna@gmail.com>
2021-05-26 12:07:37 +0530

    msg

---
+++ first15programs/Circle.java
@@ -0,0 +1,12 @@
+public class Circle
+{
+private static final double radius=7.5;
+public static void main(String args[])
+{
+double perimeter=2*Math.PI*radius;
+double area=Math.PI*radius*radius;
+
+System.out.println("perimeter is ="+perimeter);
+System.out.println("area is ="+area);
+}
+}
---
+++ first15programs/LargestNum.java
@@ -0,0 +1,14 @@
+public class LargestNum
+{
+public static void main(String args[])
+{
+int num1=34, num2=78, num3=12;
+if(num1>=num2 &&num1>=num3)
+ System.out.println(num1+" is the largest number");
+else if(num2>=num1 && num2>=num3)
+ System.out.println(num2+" is the largest number");
+else
+ System.out.println(num3+" is the largest number");
+}
+}
+ 
