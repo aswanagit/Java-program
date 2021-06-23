a23ea080c14be1d7ea8694c178edb50314dc173c
ASWANA B KRISHNA <aswanabkrishna@gmail.com>
2021-06-23 13:07:18 +0530

    msg

---
+++ co2/prgrm1/SortString.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class SortString{
+String str;
+void get_string()
+{ 
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the String:");
+str=sc.nextLine();
+}
+
+void sort()
+{
+char CharArray[]=str.toCharArray();
+Arrays.sort(CharArray);
+System.out.println("sorted string is:"+new String(CharArray));
+}
+
+public static void main(String args[])
+{
+SortString s=new SortString();
+s.get_string();
+s.sort();
+}
+}
---
+++ co2/prgrm2/SearchElement.java
@@ -0,0 +1,38 @@
+import java.util.Scanner; 
+class SearchElement{
+public static void main(String args[]) 
+  {
+     int n, x, flag = 0, i;
+     Scanner s = new Scanner(System.in);
+     System.out.print("Enter no of elements in array:");
+     n = s.nextInt();
+     int a[] = new int[n];
+     System.out.println("Enter the elements:");
+     for(i=0;i<n;i++)
+     {
+        a[i] = s.nextInt();
+      }
+      System.out.print("Enter the element to search:");
+      x = s.nextInt();
+      for(i=0;i<n;i++)
+      {
+        if(a[i] == x)
+         {
+           flag = 1;
+           break;
+          }
+         else
+          {
+             flag = 0;
+          }
+        }
+        if(flag == 1)
+        {
+            System.out.println("Element found at position:"+(i + 1));
+        }
+        else
+        {
+            System.out.println("Element not found");
+        }
+    }
+}
